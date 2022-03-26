4b629acc668b362252b7ccd149c0f771c1c41525
kyoF <fujiki.kyosuke@gmail.com>
2022-03-26 12:53:10 +0900

    空行の調整とslackテキストを出力する空メソッドを追加

--- comic.py
+++ comic.py
@@ -12,14 +12,17 @@ def main():
 
     target_url = get_url_from_json(
         'target_scraped_url').format(today.year, today.month)
-    all_comics = get_comic_info_from_html(target_url)
-    comics_list = all_comics.find_all('tr')
-    for release_date, today_comic in enumerate(comics_list):
+
+    all_comics = get_comic_info_from_html(target_url).find_all('tr')
+
+    for release_date, today_comic in enumerate(all_comics):
         if release_date == 0:
             continue
+
         if release_date == today.day:
             comics_of_day = today_comic.find(
                 'td', class_='products-td').find_all('div', class_='div-wrap')
+
             for comic in comics_of_day:
                 comic_info_dict = {
                     'title': '',
@@ -39,7 +42,7 @@ def main():
 
             break
 
-    print(comics)
+    slack_text = create_slack_text(comics)
 
 
 def get_today():
@@ -82,6 +85,10 @@ def get_author(comic):
         return comic.find_all('p', class_='p-company')[1].get_text()
     except:
         return '不明'
+
+
+def create_slack_text(comics):
+    return ''
 
 
 def slack_notify(text, month, day, day_of_week):
